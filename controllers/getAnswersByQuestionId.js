dce18a2148dc6e7ae544fbe0fd1245063a26e331
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2021-12-11 23:34:21 +0300

    commit 3: добавил проверки в контроллеры получения вопросов и ответов

--- controllers/getAnswersByQuestionId.js
+++ controllers/getAnswersByQuestionId.js
@@ -2,7 +2,18 @@ const { answers } = require('../store/answers');
 
 const getAnswersByQuestionId = (req, res) => {
   const { questionId, answerId } = req.body;
-  console.log('answerId: ', answerId);
+  if (questionId < 1) {
+    res.status(400).send({ message: 'Передан некорректный айди' });
+  }
+  if (!answers[questionId]) {
+    res.status(404).send({ message: `Вопрос с айди ${questionId} не найден` });
+  }
+  if (typeof answerId === 'undefined') {
+    res.status(400).send({ message: 'Необходимо передать айди ответа' });
+  }
+  if ((answerId < 1) || (answerId > 4)) {
+    res.status(400).send({ message: 'Передан некорректный айди ответа' });
+  }
   res.send(answers[questionId]);
 };
 
--- controllers/getQuestionById.js
+++ controllers/getQuestionById.js
@@ -2,6 +2,12 @@ const { questions } = require('../store/questions');
 
 const getQuestionById = (req, res) => {
   const { questionId } = req.body;
+  if (questionId < 1) {
+    res.status(400).send({ message: 'Передан некорректный айди' });
+  }
+  if (!questions[questionId]) {
+    res.status(404).send({ message: `Вопрос с айди ${questionId} не найден` });
+  }
   res.send(questions[questionId]);
 };
 
