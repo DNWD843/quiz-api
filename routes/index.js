11399c027e792577402cfebe8e6621643a0d9008
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2021-12-12 15:21:00 +0300

    commit 6: поправил эндпойнты

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { getQuestionById } = require('../controllers/getQuestionById');
 const { getAnswersByQuestionId } = require('../controllers/getAnswersByQuestionId');
 
-router.get('/question', getQuestionById);
-router.post('/question', getAnswersByQuestionId);
+router.get('/api/question', getQuestionById);
+router.post('/api/question', getAnswersByQuestionId);
 
 module.exports = router;
