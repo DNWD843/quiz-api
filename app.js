672ad10fc6e1d99ff30606350c46e59cb93cbf51
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2021-12-13 22:44:53 +0300

    commit 14: прикрутил статику для пробы сервера

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const path = require('path');
 const rootRouter = require('./routes');
 const handleErrors = require('./middlwares/handleErrors');
 
@@ -17,7 +18,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(rootRouter);
 app.use(handleErrors);
