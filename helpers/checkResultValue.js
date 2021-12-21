2d5b273cd9b595206698b2e91479a2fa6aa6a8c3
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2021-12-21 18:11:47 +0300

    commit 16: поправвил getResult

--- helpers/checkResultValue.js
+++ helpers/checkResultValue.js
@@ -1,5 +1,5 @@
 const questions = require('../store/questions');
 
-const checkResultValue = (result) => ((result > 0) && (result <= Object.keys(questions).length));
+const checkResultValue = (result) => ((result >= 0) && (result <= Object.keys(questions).length));
 
 module.exports = checkResultValue;
